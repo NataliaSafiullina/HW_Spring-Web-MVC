32d530fc82a818dd5c6e29c2d128b4b4990730a9
NataliaSafiullina <nfsafiullina@gmail.com>
2025-01-08 13:08:50 +0700

    Add Main

---
+++ src/main/java/ru/safiullina/Main.java
@@ -0,0 +1,41 @@
+package ru.safiullina;
+
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.startup.Tomcat;
+
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class Main {
+
+    public static void main(String[] args) throws LifecycleException, IOException {
+        // Конфигурируем Tomcat: создаем объект класса Tomcat
+        final var tomcat = new Tomcat();
+        // Создаем для Томкэта временную директорию
+        final var baseDir = Files.createTempDirectory("tomcat");
+        // Директория будет временной
+        baseDir.toFile().deleteOnExit();
+
+        // Сеттером делаем настройку Томкэты
+        tomcat.setBaseDir(baseDir.toAbsolutePath().toString());
+
+        // Настраиваем порты через коннекторы, создаем коннектор
+        final var connector = new Connector();
+        // Устанавливаем порт 9999
+        connector.setPort(9999);
+        // Присваиваем Томкэту коннектор
+        tomcat.setConnector(connector);
+
+        // Базовый путь к приложению будет текущая папка
+        tomcat.getHost().setAppBase(".");
+        // Контекст пустой и папка для документов тоже текущая
+        tomcat.addWebapp("", ".");
+
+        // Стартуем сервер и чтобы приложение не закрылось вызываем метод await()
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+
+
+}
